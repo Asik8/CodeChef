4c3efa39f7d754f8a175e59c6e56d94f048bddfc
Asik8 <motasembillah84@gmail.com>
2024-05-22 20:33:57 +0600

    First added

---
+++ RCB_vs_CSK.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    if(x-y>= 18) cout<<"RCB\n";
+    else cout<<"CSK\n";
+    return 0;
+}
