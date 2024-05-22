15a274dd91fa90c6ce9158057dda989c24bc7447
Asik8 <motasembillah84@gmail.com>
2024-05-22 20:42:09 +0600

    2nd done

---
+++ Football_Ties.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x,y;
+        cin>>x>>y;
+        x%=3;
+        cout<<x<<endl;
+    }
+
+    return 0;
+}
