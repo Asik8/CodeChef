7cc97e0e846c0ba910d2c834ab55d8a2726b4ace
Asik8 <motasembillah84@gmail.com>
2024-06-19 20:33:15 +0600

    1st done

---
+++ Television_Channels.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    int c=0;
+    for (int i = 1; i <= t; i+=2)
+        c++;
+    cout<<c<<endl;
+    return 0;
+}
