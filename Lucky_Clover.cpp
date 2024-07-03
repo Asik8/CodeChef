2728f5a9a6ba76ddaaf8a31e7b1fbce910348ea1
Asik8 <motasembillah84@gmail.com>
2024-07-03 20:36:41 +0600

    3rd july 1st done

---
+++ Lucky_Clover.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pob pop_back
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    cout<<(t-1)*3+4<<endl;
+
+    return 0;
+}
