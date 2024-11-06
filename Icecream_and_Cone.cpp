c2f58949fca25e09ed7ba29fb741c81c95ea7106
Asik8 <motasembillah84@gmail.com>
2024-11-06 20:31:39 +0600

    6th November 2024 1 done

---
+++ Icecream_and_Cone.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define forni for (int i = 0; i < n; i++)
+#define fornj for (int j = 0; j < n; j++)
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll a,b;
+    cin>>a>>b;
+    cout<<min(a,b)<<endl;
+    return 0;
+}
