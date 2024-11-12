e3efbb47fb900e351da32d9df9372ceecb3c7c49
Asik8 <motasembillah84@gmail.com>
2024-11-12 21:15:22 +0600

    12th November 2024 3 done

---
+++ Vaccine_Dates.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll d,l,r;
+        cin>>d>>l>>r;
+        if(d<l) cout<<"Too Early\n";
+        else if(d>r) cout<<"Too Late\n";
+        else cout<<"Take second dose now\n";       
+    }
+    return 0;
+}
