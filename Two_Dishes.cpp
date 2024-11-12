7059aefcff5bb41c37c846820f68dbefb40c9759
Asik8 <motasembillah84@gmail.com>
2024-11-12 21:41:39 +0600

    12th November 2024 6 done

---
+++ Two_Dishes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n,a,b,c;
+        cin >>n>>a>>b>>c;
+        if(n<=(a+c) && n<=b) py else pn       
+    }
+    return 0;
+}
