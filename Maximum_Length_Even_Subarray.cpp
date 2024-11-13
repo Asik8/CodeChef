a3c3e593a48c1d695cd011c65e8b1239ddaac47d
Asik8 <motasembillah84@gmail.com>
2024-11-13 09:02:16 +0600

    13th November 2024 5 done

---
+++ Maximum_Length_Even_Subarray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        ll l=n/2;
+        ll o=(n/2)+(n%2);
+        if(o%2) o--;
+        l+=o;
+        cout<<l<<endl;
+    }
+    return 0;
+}
