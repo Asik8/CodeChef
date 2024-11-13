47e1e8ca2fff444ad278c108badf515b5e03268f
Asik8 <motasembillah84@gmail.com>
2024-11-13 21:03:39 +0600

    13th November 2024 8 done

---
+++ Movie_Snacks.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll x,y,z;
+    cin>>x>>y>>z;
+    if(x+y<=z) cout<<(x+y)*2+y<<endl;
+    else cout<<z*2+y<<endl;
+    return 0;
+}
