ab735e89a174a8b2aca1fadc6af024ad8d9306d0
Asik8 <motasembillah84@gmail.com>
2024-11-13 08:07:21 +0600

    13th November 2024 2 done

---
+++ Weight_Balance.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll w1,w2,x1,x2,m;
+        cin>>w1>>w2>>x1>>x2>>m;   
+        ll a=(w2-w1)/m;
+        if(a<x1 || a>x2) cout<<"0\n";    
+        else cout<<"1\n";    
+    }
+    return 0;
+}
