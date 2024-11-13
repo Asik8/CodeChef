6f30cb13463cc944b34cb7daf5859cd7f47979d0
Asik8 <motasembillah84@gmail.com>
2024-11-13 10:20:10 +0600

    13th November 2024 7 done

---
+++ Distinct_Pair_Sums.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll a,b;
+        cin>>a>>b;
+        cout<<(b-a)*2+1<<endl;       
+    }
+    return 0;
+}
