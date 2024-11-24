76fdced97e2b7f1ba08478b93cc6f05f00c54145
Asik8 <motasembillah84@gmail.com>
2024-11-24 08:26:14 +0600

    24th November 2024 3 done

--- Two_Cards.cpp
+++ Two_Cards.cpp
@@ -16,8 +16,25 @@ int main() {
     while (t--) {
         ll n;
         cin >> n;
-        vector <ll> v(n);
-        for (auto& x:v) cin >>x;       
+        vector <ll> a(n),b(n);
+        for (auto& x:a) cin >>x;       
+        for (auto& x:b) cin >>x; 
+        map<ll,ll>mp;
+        forni mp[a[i]]=max(a[i],b[i]);
+        sort(a.begin(),a.end(),greater<ll>());
+        ll c=mp[a[0]];
+        bool f=false;
+        for(int i=1;i<n;i++){
+            if(mp[a[i]]>c){
+                f=true;
+                break;
+            }
+        }
+        if(f) py
+        else{
+            if(c>mp[a[1]]) py
+            else pn
+        }
     }
     return 0;
 }
