3a69d9ccfef5c23a5b71fc9c246a2de25f1bc3c8
Asik8 <motasembillah84@gmail.com>
2024-11-27 20:33:38 +0600

    27th November 2024 1 done

---
+++ Calorie_Intake.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll a,b,c;
+    cin>>a>>b>>c;
+    if((b*c)>a) co(-1)
+    else co(a-(b*c))       
+    return 0;
+}
