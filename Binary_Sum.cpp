728e2af3a90442f7ec5fbf1e4f88cbb12ef0353b
Asik8 <motasembillah84@gmail.com>
2024-12-04 20:44:36 +0600

    4th December 2024 2 done

---
+++ Binary_Sum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n,k;
+        cin >> n>>k;
+        if((n/2)==k || ((n+1)/2)==k) py else pn     
+    }
+    return 0;
+}
