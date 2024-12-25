b8e17c82a08395764771ff0303f4d8631ed66bcc
Asik8 <motasembillah84@gmail.com>
2024-12-25 20:45:25 +0600

    25th December 2024 3 done

---
+++ Chefland_Library.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define sz(x) x.size()
+#define vec vector<ll>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    map<ll,ll>mp;
+    forni{
+        ll x;
+        cin>>x;
+        mp[x]=(i+1);
+    }
+    ll c=0;
+    for(auto x:mp) c+=x.second;
+    co(c)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        asikM();      
+    }
+    return 0;
+}
