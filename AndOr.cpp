0475782bbf2194413249af24cc588db465dab41d
Asik8 <motasembillah84@gmail.com>
2025-02-06 12:36:59 +0600

    6th Februrary 2025 2 done

---
+++ AndOr.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    set<ll,greater<ll>>s;
+    forni if((i+1)%2 == v[i]) s.pbs(i);
+    ll q;
+    cin>>q;
+    while(q--){
+        ll p,x;
+        cin>>p>>x;
+        p--;
+        if(v[p]!=x){
+            v[p]=x;
+            if(s.count(p)) s.erase(p);
+            else s.insert(p);
+        }
+        if(s.empty()) co(v[0])
+        else co(v[*s.begin()])
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
