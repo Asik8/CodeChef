b3b7a095023846d7fad08ed3987e530d4d19849b
Asik8 <motasembillah84@gmail.com>
2025-02-06 11:38:28 +0600

    6th Februrary 2025 1 done

---
+++ Transform_String.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    string s1,s2;
+    cin>>s1>>s2;
+    ll c=0,n=sz(s1),m=sz(s2),l=m-1;
+    fr(i,n-1,0){
+        if(l>=0 && s2[l]==s1[i]) l--;
+        else c+=l+2;
+    }
+    if(l>=0){
+        co(-1)
+        return;
+    }
+    co(c)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
+
