d30812852e924318dc03c4499e312dc15e7fcdb8
Asik8 <motasembillah84@gmail.com>
2025-03-30 02:58:00 +0600

    30th March 2025 3 done

---
+++ Running_Comparison.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,c=0;
+    cin >> n;
+    vector <ll> a(n),b(n);
+    for (auto& x:a) cin >>x; 
+    for (auto& x:b) cin >>x; 
+    forni{
+        bool f1=true,f2=true;
+        if(b[i]>2*a[i]) f1=false;
+        if(a[i]>2*b[i]) f2=false;
+        if(f1 && f2) c++;
+    }
+    co(c)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
