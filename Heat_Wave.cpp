e49f524097ee056e49d5eed218f058a270ed9d76
Asik8 <motasembillah84@gmail.com>
2025-03-30 17:48:15 +0600

    30th March 2025 6 done

---
+++ Heat_Wave.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define forni for (int i = 0; i < n; i++)
+#define fornj for (int j = 0; j < n; j++)
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll a,b;
+    cin>>a>>b;
+    if(b>a) py
+    else pn       
+    return 0;
+}
