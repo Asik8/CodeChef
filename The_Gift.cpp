a7da40b20a00dde14df65e80853cd62e0b742c97
Asik8 <motasembillah84@gmail.com>
2025-04-01 00:02:11 +0600

    31th March 2025 7 done

---
+++ The_Gift.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll a,b,c;
+    cin>>a>>b>>c;
+    if((a+c)>=b)py else pn
+    return 0;
+}
