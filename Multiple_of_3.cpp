6e3abd404ace77570aafc5de7005d4c785373ab8
Asik8 <motasembillah84@gmail.com>
2025-04-30 22:37:29 +0600

    30th April 2025 3 done

---
+++ Multiple_of_3.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    if(!(n%3)){
+        co(n)
+        return;
+    } 
+    ll c1=0,c2=0,l=n,r=n;
+    while(1){
+        if(!(l%3)) break;
+        c1++;
+        l--;
+    }
+    while(1){
+        if(!(r%3)) break;
+        c2++;
+        r++;
+    }
+    // cout<<c1<<" "<<c2<<el
+    if(c1<=c2) co(n-c1) else co(n+c2)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    // cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
