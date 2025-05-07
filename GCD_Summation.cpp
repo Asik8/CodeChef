edf548b87cae36c94e2b5f86e4a32b5b5d8b1461
Asik8 <motasembillah84@gmail.com>
2025-05-07 21:15:17 +0600

    7th May 2025 2 done

---
+++ GCD_Summation.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,k;
+    cin >> n>>k;
+    if(k<n-1){
+        co(-1)
+        return;
+    }
+    if(n==2){
+        cout<<k<<" "<<k*2<<el
+        return;
+    }
+    if(n-1==k){
+        ll l=1;
+        forni{
+            ct(l)
+            l+=2;
+        }
+        elc
+        return;
+    }
+    vector<ll> ans;
+    set<ll>s;
+    ll l=k-(n-2);
+    ans.pb(l); s.insert(l);
+    ans.pb(2*l); s.insert(l*2);
+    l=1;
+    fl(i,0,n-2){
+        while(s.count(l)) l+=2;
+        ans.pb(l);
+        l+=2;
+    }
+    flx(ans)
+    // pn
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
