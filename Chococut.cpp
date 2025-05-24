20788d20d12f902a546839e66b54cce08452e0e9
Asik8 <motasembillah84@gmail.com>
2025-05-24 22:52:27 +0600

    24th May 2025 1 done

---
+++ Chococut.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,m,k;
+    cin >> n>>m>>k;
+    ll l=(k+(n-1))/n,r=(k+(m-1))/m;
+    cout<<l<<" "<<r<<el
+    if(l==r){
+        ll x=(n-l);
+        x*=m;
+        ll y=(m-r);
+        y*=n;
+        co(max(x,y))
+    } else if(l<r){
+        
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
